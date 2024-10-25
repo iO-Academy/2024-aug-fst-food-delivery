c19d7626ed128d63e8f1a3c522b6c78a37ec594e
Matt_Mannings <matt.mannings97@gmail.com>
2024-10-25 11:28:52 +0100

    working basket array and menucard counter in sync

--- src/Components/OrderQuantityButton/index.jsx
+++ src/Components/OrderQuantityButton/index.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function OrderQuantityButton( { basket, setBasket, foodItem } ) {
+  const [updatedItem, setItem] = useState(0)
 
   function handleIncrement() {
     const found = basket.filter((item) => item.name !== foodItem.foodName)
@@ -9,8 +10,10 @@ function OrderQuantityButton( { basket, setBasket, foodItem } ) {
       let updatedItem = match.length > 0 && match[0]
       if(match.length > 0 ) {
       updatedItem.count += 1
+      setItem(updatedItem.count)
         setBasket([...found, updatedItem])
       } else {
+        setItem(1)
         setBasket([...basket, {name: foodItem.foodName, count: 1}])
       }
     }
@@ -23,9 +26,11 @@ function OrderQuantityButton( { basket, setBasket, foodItem } ) {
       let updatedItem = match.length > 0 && match[0]
       if(match.length > 0 ) {
         if(updatedItem.count === 1) {
+          setItem(0)
           setBasket([...found])
         } else {
           updatedItem.count -= 1
+          setItem(updatedItem.count)
           setBasket([...found, updatedItem])
         }
       } else {
@@ -43,7 +48,7 @@ function OrderQuantityButton( { basket, setBasket, foodItem } ) {
       >
         -
       </button>
-      <span className="font-extrabold text-xs p-2"></span>
+      <span className="font-extrabold text-xs p-2">{updatedItem}</span>
       <button
         onClick={handleIncrement}
         className="bg-blue-500 text-white h-8 w-7 rounded"
